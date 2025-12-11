d8e5fe0480005842387fb8c1b477f8489822ab57
KoinGame <obezet12@gmail.com>
2025-12-11 23:49:41 +0700

    chore: mqtt broker change from hivemq to emqx

--- esp32-mqtt/src/main.cpp
+++ esp32-mqtt/src/main.cpp
@@ -2,40 +2,54 @@
 #include <PubSubClient.h>
 
 const char* ssid = "YOUR_WIFI";
-const char* password = "YOUR_PASSWORD";
+const char* password = "YOUR_WIFI_PASSWORD";
 
-const char* mqtt_server = "broker.hivemq.com";
+const char* mqtt_server = "q8a52061.ala.dedicated.aws.emqxcloud.com";
 const int mqtt_port = 1883;
+
+const char* mqtt_user = "khoinm_123";
+const char* mqtt_pass = "123456";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void callback(char* topic, byte* message, unsigned int length) {
-  Serial.print("Received: ");
-  Serial.println(topic);
+  Serial.print("Message [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+  }
+  Serial.println();
 }
 
 void setup_wifi() {
+  delay(10);
+  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
+
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("WiFi connected!");
+
+  Serial.println("\nWiFi connected!");
 }
 
 void reconnect() {
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect("esp32-client")) {
-      Serial.println("connected");
-      client.subscribe("esp32/test");
+    Serial.print("Connecting to MQTT...");
+    
+    // connect(clientID, username, password)
+    if (client.connect("esp32-client", mqtt_user, mqtt_pass)) {
+      Serial.println("connected!");
+      client.subscribe("petcarex/command");
     } else {
       Serial.print("failed, rc=");
-      Serial.print(client.state());
+      Serial.println(client.state());
       delay(2000);
     }
   }
@@ -54,7 +68,7 @@ void loop() {
     reconnect();
   }
   client.loop();
-  
-  client.publish("esp32/test", "Hello from ESP32");
+
+  client.publish("petcarex/status", "ESP32 Alive!");
   delay(3000);
 }
