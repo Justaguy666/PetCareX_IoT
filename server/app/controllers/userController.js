35aa9bd4c0c336b796048e44ac5fa220938e7f20
KoinGame <obezet12@gmail.com>
2025-12-14 23:38:50 +0700

    small changes

---
+++ server/app/controllers/userController.js
@@ -0,0 +1,49 @@
+import User from '../models/User.js';
+
+class UserController {
+    async getNewestFeeding(req, res) {
+
+    }
+
+    async getNextFeeding(req, res) {
+
+    }
+
+    async createFeedingSchedule(req, res) {
+
+    }
+
+    async updateFeedingSchedule(req, res) {
+
+    }
+
+    async deleteFeedingSchedule(req, res) {
+
+    }
+
+    async getFeedingStatistics(req, res) {
+
+    }
+
+    async getWeeklyReport(req, res) {
+
+    }
+
+    async getSettings(req, res) {
+
+    }
+
+    async updateSettings(req, res) {
+
+    }
+
+    async getUserProfile(req, res) {
+        
+    }
+
+    async updateUserProfile(req, res) {
+
+    }
+}
+
+export default new UserController();
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,10 @@
 import authRouter from './auth.js';
-//import userRoutes from './user.route.js';
+import userRoutes from './user.route.js';
 import chatbotRouter from './chatbot.js';
 
 export default function route(app) {
     app.use('/api/auth', authRouter);
-    //app.use('/api/users', userRoutes);
+    app.use('/api/users', userRoutes);
     app.use('/api/chatbot', chatbotRouter);
     app.use('/', (req, res) => {
         return res.json({message: "Hello"});
