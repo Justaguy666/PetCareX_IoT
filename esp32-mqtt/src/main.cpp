b9a188ef173710b2f95984b034295c1c82e979fe
KoinGame <obezet12@gmail.com>
2025-12-16 09:58:09 +0700

    mock code for esp32

--- esp32-mqtt/src/main.cpp
+++ esp32-mqtt/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 #include <ESP32Servo.h>
 
 /* ================= PIN ================= */
 #define TRIG_WATER   4
 #define ECHO_WATER   16
-
 #define TRIG_FOOD    5
 #define ECHO_FOOD    18
 
@@ -23,18 +23,18 @@
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 
-/* ================= MQTT ================= */
-const char* mqtt_server = "q8a52061.ala.dedicated.aws.emqxcloud.com";
-const int   mqtt_port   = 1883;
-const char* mqtt_user   = "vinhok_123";
-const char* mqtt_pass   = "123456";
+/* ================= MQTT (HiveMQ) ================= */
+const char* mqtt_server = "bb2c782916b34e689328539f4439a1b2.s1.eu.hivemq.cloud";
+const int   mqtt_port   = 8883;
+const char* mqtt_user   = "petcare";
+const char* mqtt_pass   = "Petcare123";
 
 #define TOPIC_STATUS   "petcarex/status"
 #define TOPIC_SENSOR   "petcarex/sensor"
 #define TOPIC_COMMAND  "petcarex/command"
 
 /* ================= OBJECT ================= */
-WiFiClient espClient;
+WiFiClientSecure espClient;
 PubSubClient client(espClient);
 Servo feeder;
 
@@ -48,7 +48,6 @@ float readDistanceCM(int trigPin, int echoPin) {
 
   long duration = pulseIn(echoPin, HIGH, 30000);
   if (duration == 0) return -1;
-
   return duration * 0.034 / 2;
 }
 
@@ -60,7 +59,7 @@ int calcPercent(float distance, float height) {
 }
 
 void feedPet() {
-  Serial.println("Feeding pet...");
+  Serial.println("🐾 Feeding pet...");
   feeder.write(90);
   delay(1500);
   feeder.write(0);
@@ -69,7 +68,6 @@ void feedPet() {
 void setup_wifi() {
   Serial.print("Connecting WiFi");
   WiFi.begin(ssid, password);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -79,9 +77,7 @@ void setup_wifi() {
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
   String msg;
-  for (int i = 0; i < length; i++) {
-    msg += (char)payload[i];
-  }
+  for (int i = 0; i < length; i++) msg += (char)payload[i];
 
   Serial.print("MQTT [");
   Serial.print(topic);
@@ -89,27 +85,21 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
   Serial.println(msg);
 
   if (String(topic) == TOPIC_COMMAND) {
-    if (msg == "FEED") {
-      feedPet();
-    }
-    if (msg == "PUMP_ON") {
-      digitalWrite(RELAY_PIN, HIGH);
-    }
-    if (msg == "PUMP_OFF") {
-      digitalWrite(RELAY_PIN, LOW);
-    }
+    if (msg == "FEED") feedPet();
+    if (msg == "PUMP_ON") digitalWrite(RELAY_PIN, HIGH);
+    if (msg == "PUMP_OFF") digitalWrite(RELAY_PIN, LOW);
   }
 }
 
 void reconnectMQTT() {
   while (!client.connected()) {
-    Serial.print("Connecting MQTT...");
+    Serial.print("Connecting to HiveMQ...");
     if (client.connect("esp32-petcarex", mqtt_user, mqtt_pass)) {
-      Serial.println("connected");
+      Serial.println(" connected");
       client.subscribe(TOPIC_COMMAND);
       client.publish(TOPIC_STATUS, "ESP32 Online");
     } else {
-      Serial.print("failed, rc=");
+      Serial.print(" failed, rc=");
       Serial.println(client.state());
       delay(2000);
     }
@@ -129,56 +119,43 @@ void setup() {
   pinMode(LED_STATUS, OUTPUT);
   pinMode(BTN_FEED, INPUT_PULLUP);
 
-  digitalWrite(RELAY_PIN, LOW);
-  digitalWrite(LED_STATUS, HIGH);
-
   feeder.attach(SERVO_PIN);
   feeder.write(0);
 
   setup_wifi();
 
+  espClient.setInsecure();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(mqttCallback);
 
-  Serial.println("PetCareX ESP32 started");
+  Serial.println("🚀 PetCareX ESP32 started (HiveMQ)");
 }
 
 /* ================= LOOP ================= */
 void loop() {
-  if (!client.connected()) {
-    reconnectMQTT();
-  }
+  if (!client.connected()) reconnectMQTT();
   client.loop();
 
-  /* ---- Read sensors ---- */
   float distWater = readDistanceCM(TRIG_WATER, ECHO_WATER);
   float distFood  = readDistanceCM(TRIG_FOOD, ECHO_FOOD);
 
   int waterPercent = calcPercent(distWater, TANK_HEIGHT_CM);
   int foodPercent  = calcPercent(distFood, FOOD_HEIGHT_CM);
 
-  /* ---- Publish sensor ---- */
-  char payload[100];
+  char payload[64];
   snprintf(payload, sizeof(payload),
            "{\"water\":%d,\"food\":%d}",
            waterPercent, foodPercent);
 
   client.publish(TOPIC_SENSOR, payload);
 
-  /* ---- Auto pump ---- */
-  if (waterPercent < 30) {
-    digitalWrite(RELAY_PIN, HIGH);
-  } else {
-    digitalWrite(RELAY_PIN, LOW);
-  }
+  digitalWrite(RELAY_PIN, waterPercent < 30 ? HIGH : LOW);
 
-  /* ---- Auto feed ---- */
   if (foodPercent < 20) {
     feedPet();
     delay(5000);
   }
 
-  /* ---- Manual feed ---- */
   if (digitalRead(BTN_FEED) == LOW) {
     feedPet();
     delay(2000);
